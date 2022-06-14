f05541deb95fe3f4555554e709ff3e903874a9f4
Murandido <murilo.candidomc97@gmail.com>
2022-06-14 16:40:08 -0300

    Index teste

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <div>
+            <h1>Olá mundo!</h1>
+            <p>Sou uma página de teste com NextJS, se você está vendo, que bom!</p>
+        </div>
+    );
+}
+
+export default HomePage;
