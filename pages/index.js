0ddc638a14e44be848052181f070efebf0ca12f7
Murandido <murilo.candidomc97@gmail.com>
2022-06-14 16:54:26 -0300

    imagens

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,11 @@ function HomePage() {
         <div>
             <h1>Olá mundo!</h1>
             <p>Sou uma página de teste com NextJS, se você está vendo, que bom!</p>
-            <h1>O guri ta duvidadando, mas o site é do Murilo vulgo eu</h1>
+            <h1>O guri ta duvidando, mas o site é do Murilo vulgo eu</h1>
             <p>Ó guri gordo n duvide, é us guri</p>
+            <img src="https://play-lh.googleusercontent.com/5B6Z4GWPuAlrnZDtqI8tdlnngu4dg-QG3lD2OTIvyvzSGCRJibEDaAbvQr6TMCPjqUc" alt="amogus" />
+            <img src="https://super.abril.com.br/wp-content/uploads/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png" alt="catioro" />
+            <img src="https://img.estadao.com.br/fotos/crop/1200x1200/resources/jpg/1/3/1481047107431.jpg" alt="pidipie" />
         </div>
     );
 }
