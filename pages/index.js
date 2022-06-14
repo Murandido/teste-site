0a1dc770ef9f76d58cc66fc8e3c61d484e195efe
Murandido <murilo.candidomc97@gmail.com>
2022-06-14 16:49:30 -0300

    guri

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ function HomePage() {
         <div>
             <h1>Olá mundo!</h1>
             <p>Sou uma página de teste com NextJS, se você está vendo, que bom!</p>
+            <h1>O guri ta duvidadando, mas o site é do Murilo vulgo eu</h1>
+            <p>Ó guri gordo n duvide, é us guri</p>
         </div>
     );
 }
